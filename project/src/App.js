93b9fa38bbfab49217a8fc7d067366b3092b03ec
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-06-22 13:37:16 +0700

    first up

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
